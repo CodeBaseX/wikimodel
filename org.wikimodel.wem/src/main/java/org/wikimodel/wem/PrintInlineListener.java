c67a10963e8b97a62de1e066f0ee5367735e1d67
mikhail.kotelnikov <mikhail.kotelnikov@912f9990-9e31-0410-84af-1fc73436fe80>
2007-11-30 12:21:33 +0000

    Tex file generation was refactored (image support was added with image size definition) + image manipulation library was added

--- org.wikimodel.wem/src/main/java/org/wikimodel/wem/PrintInlineListener.java
+++ org.wikimodel.wem/src/main/java/org/wikimodel/wem/PrintInlineListener.java
@@ -7,101 +7,104 @@ import org.wikimodel.wem.util.WikiEntityUtil;
  */
 public class PrintInlineListener extends PrintTextListener {
 
-    /**
-     * 
-     */
-    public PrintInlineListener(IWikiPrinter printer) {
-        super(printer);
-    }
+	/**
+	 * 
+	 */
+	public PrintInlineListener(IWikiPrinter printer) {
+		super(printer);
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#beginFormat(org.wikimodel.wem.WikiFormat)
-     */
-    public void beginFormat(WikiFormat format) {
-        print(format.getTags(true));
-    }
+	/**
+	 * @see org.wikimodel.wem.IWemListener#beginFormat(org.wikimodel.wem.WikiFormat)
+	 */
+	public void beginFormat(WikiFormat format) {
+		print(format.getTags(true));
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#beginPropertyInline(java.lang.String)
-     */
-    public void beginPropertyInline(String str) {
-        print("<span class='property' url='"
-            + WikiPageUtil.escapeXmlAttribute(str)
-            + "'>");
-    }
+	/**
+	 * @see org.wikimodel.wem.IWemListener#beginPropertyInline(java.lang.String)
+	 */
+	public void beginPropertyInline(String str) {
+		print("<span class='property' url='"
+				+ WikiPageUtil.escapeXmlAttribute(str) + "'>");
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#endFormat(org.wikimodel.wem.WikiFormat)
-     */
-    public void endFormat(WikiFormat format) {
-        print(format.getTags(false));
-    }
+	/**
+	 * @see org.wikimodel.wem.IWemListener#endFormat(org.wikimodel.wem.WikiFormat)
+	 */
+	public void endFormat(WikiFormat format) {
+		print(format.getTags(false));
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#endPropertyInline(java.lang.String)
-     */
-    public void endPropertyInline(String inlineProperty) {
-        print("</span>");
-    }
+	/**
+	 * @see org.wikimodel.wem.IWemListener#endPropertyInline(java.lang.String)
+	 */
+	public void endPropertyInline(String inlineProperty) {
+		print("</span>");
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#onEscape(java.lang.String)
-     */
-    public void onEscape(String str) {
-        print("<span class='escaped'>"
-            + WikiPageUtil.escapeXmlString(str)
-            + "</span>");
-    }
+	@Override
+	protected ReferenceHandler newReferenceHandler() {
+		return new ReferenceHandler() {
 
-    public void onExtensionInline(String extensionName, WikiParameters params) {
-        print("<span class='extension' extension='"
-            + extensionName
-            + "' "
-            + params
-            + " />");
-    }
+			@Override
+			protected void handleImage(String ref, String label) {
+				print("<img src='" + WikiPageUtil.escapeXmlAttribute(ref)
+						+ "' title='" + WikiPageUtil.escapeXmlAttribute(label)
+						+ "'/>");
+			}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#onLineBreak()
-     */
+			@Override
+			protected void handleReference(String ref, String label) {
+				print("<a href='" + WikiPageUtil.escapeXmlAttribute(ref) + "'>"
+						+ WikiPageUtil.escapeXmlString(ref) + "</a>");
+			}
 
-    public void onLineBreak() {
-        print("<br />");
-    }
+		};
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#onReference(java.lang.String)
-     */
+	/**
+	 * @see org.wikimodel.wem.IWemListener#onEscape(java.lang.String)
+	 */
+	public void onEscape(String str) {
+		print("<span class='escaped'>" + WikiPageUtil.escapeXmlString(str)
+				+ "</span>");
+	}
 
-    public void onReference(String ref) {
-        print("<a href='"
-            + WikiPageUtil.escapeXmlAttribute(ref)
-            + "'>"
-            + WikiPageUtil.escapeXmlString(ref)
-            + "</a>");
-    }
+	public void onExtensionInline(String extensionName, WikiParameters params) {
+		print("<span class='extension' extension='" + extensionName + "' "
+				+ params + " />");
+	}
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#onSpecialSymbol(java.lang.String)
-     */
-    public void onSpecialSymbol(String str) {
-        String entity = WikiEntityUtil.getHtmlSymbol(str);
-        if (entity != null) {
-            entity = "&" + entity + ";";
-            if (str.startsWith(" --")) {
-                entity = "&nbsp;" + entity + " ";
-            }
-        } else {
-            entity = WikiPageUtil.escapeXmlString(str);
-        }
-        print(entity);
-    }
+	/**
+	 * @see org.wikimodel.wem.IWemListener#onLineBreak()
+	 */
 
-    /**
-     * @see org.wikimodel.wem.IWemListener#onVerbatimInline(java.lang.String)
-     */
-    public void onVerbatimInline(String str) {
-        print("<code>" + WikiPageUtil.escapeXmlString(str) + "</code>");
-    }
+	public void onLineBreak() {
+		print("<br />");
+	}
+
+	/**
+	 * @see org.wikimodel.wem.IWemListener#onSpecialSymbol(java.lang.String)
+	 */
+	public void onSpecialSymbol(String str) {
+		String entity = WikiEntityUtil.getHtmlSymbol(str);
+		if (entity != null) {
+			entity = "&" + entity + ";";
+			if (str.startsWith(" --")) {
+				entity = "&nbsp;" + entity + " ";
+			}
+		} else {
+			entity = WikiPageUtil.escapeXmlString(str);
+		}
+		print(entity);
+	}
+
+	/**
+	 * @see org.wikimodel.wem.IWemListener#onVerbatimInline(java.lang.String)
+	 */
+	public void onVerbatimInline(String str) {
+		print("<code>" + WikiPageUtil.escapeXmlString(str) + "</code>");
+	}
 
 }
