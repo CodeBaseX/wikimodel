13efab0120bd03b19d4aa55b77fc013bdb48d43a
thomas.mortagne <thomas.mortagne@912f9990-9e31-0410-84af-1fc73436fe80>
2009-05-12 17:18:29 +0000

    WIKIMODEL-139: XHTMLParser - Lists in embedded documents in list items don't preserve their list level on parsing from xhtml

--- org.wikimodel.wem/src/main/java/org/wikimodel/wem/xhtml/handler/ListItemTagHandler.java
+++ org.wikimodel.wem/src/main/java/org/wikimodel/wem/xhtml/handler/ListItemTagHandler.java
@@ -18,38 +18,38 @@ import org.wikimodel.wem.xhtml.impl.XhtmlHandler.TagStack.TagContext;
  */
 public class ListItemTagHandler extends TagHandler {
 
-	public ListItemTagHandler() {
-		super(true, false, true);
-	}
+    public ListItemTagHandler() {
+        super(true, false, true);
+    }
 
-	public ListItemTagHandler(boolean documentContainer, boolean requiresDocument, boolean contentContainer)
-	{
-	    super(documentContainer, requiresDocument, contentContainer);
-	}
-	
+    public ListItemTagHandler(boolean documentContainer,
+            boolean requiresDocument, boolean contentContainer) {
+        super(documentContainer, requiresDocument, contentContainer);
+    }
+
     @Override
     public void begin(TagContext context) {
-        String markup = context.getParent().getName().equals("ol")
-            ? "#"
-            : "*";
+        String markup = context.getParent().getName().equals("ol") ? "#" : "*";
         begin(markup, context);
+    }
+
+    protected void begin(String markup, TagContext context) {
+        StringBuffer listStyles = (StringBuffer) context.getTagStack()
+                .getStackParameter("listStyles");
+        listStyles.append(markup);
+        context.getScannerContext().beginListItem(listStyles.toString());
     }
 
     @Override
     public void end(TagContext context) {
-        StringBuffer listStyles = (StringBuffer) context.getTagStack().getStackParameter("listStyles");
-        // We should always have a length greater than 0 but we handle 
+        StringBuffer listStyles = (StringBuffer) context.getTagStack()
+                .getStackParameter("listStyles");
+        // We should always have a length greater than 0 but we handle
         // the case where the user has entered some badly formed HTML
         if (listStyles.length() > 0) {
             listStyles.setLength(listStyles.length() - 1);
         }
-        // Note: Do not generate an endListItem() event since it'll be generated automatically by the next element.
-    }
-    
-    protected void begin(String markup, TagContext context)
-    {
-        StringBuffer listStyles = (StringBuffer) context.getTagStack().getStackParameter("listStyles");
-        listStyles.append(markup);
-        context.getScannerContext().beginListItem(listStyles.toString());
+        // Note: Do not generate an endListItem() event since it'll be generated
+        // automatically by the next element.
     }
 }
