c9f712e735d365edd6f619577620bb5fe55f9895
thomas.mortagne <thomas.mortagne@912f9990-9e31-0410-84af-1fc73436fe80>
2010-05-20 19:23:10 +0000

    Issue 182:  	 XHTML parser ignores attributes of wikimodel-freestanding images

--- trunk/org.wikimodel.wem/src/main/java/org/wikimodel/wem/xhtml/handler/ReferenceTagHandler.java
+++ trunk/org.wikimodel.wem/src/main/java/org/wikimodel/wem/xhtml/handler/ReferenceTagHandler.java
@@ -35,8 +35,10 @@ public class ReferenceTagHandler extends TagHandler {
 
     @Override
     protected void end(TagContext context) {
+        WikiParameters parameters = context.getParams();
+
         // TODO: it should be replaced by a normal parameters
-        WikiParameter ref = context.getParams().getParameter("href");
+        WikiParameter ref = parameters.getParameter("href");
 
         if (ref != null) {
             // Check if there's a class attribute with a
@@ -47,26 +49,47 @@ public class ReferenceTagHandler extends TagHandler {
             } else {
                 String content = context.getContent();
 
-                // Make sure we remove the HREF parameters from the list
-                // of parameters since they it's already consumed as link reference.
-                WikiParameters parameters = context.getParams();
-                parameters = parameters.remove("href");
-
                 WikiReference reference = new WikiReference(
                     ref.getValue(),
                     content,
-                    parameters);
+                    removeMeaningfulParameters(parameters));
                 context.getScannerContext().onReference(reference);
             }
         }
     }
 
     protected boolean isFreeStandingReference(TagContext context) {
+        WikiParameters parameters = context.getParams();
+
         // Check if there's a class attribute with a "wikimodel-freestanding"
         // value.
         // If so it means we have a free standing link.
         WikiParameter classParam = context.getParams().getParameter("class");
-        return ((classParam != null) && classParam.getValue().equalsIgnoreCase(
+
+        boolean isFreeStanding = ((classParam != null) && classParam.getValue().equalsIgnoreCase(
             "wikimodel-freestanding"));
+
+        if (isFreeStanding) {
+            parameters = removeMeaningfulParameters(parameters);
+
+            return parameters.getSize() == 0;
+        } else {
+            return false;
+        }
+    }
+
+    protected WikiParameters removeFreestanding(WikiParameters parameters) {
+        WikiParameter classParam = parameters.getParameter("class");
+        boolean isFreeStanding = ((classParam != null) && classParam.getValue().equalsIgnoreCase(
+            "wikimodel-freestanding"));
+        if (isFreeStanding) {
+            parameters = parameters.remove("class");
+        }
+
+        return parameters;
+    }
+
+    protected WikiParameters removeMeaningfulParameters(WikiParameters parameters) {
+        return removeFreestanding(parameters).remove("href");
     }
 }
